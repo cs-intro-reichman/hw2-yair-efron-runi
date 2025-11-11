2b13c5a6446391d55f96b127f6cf8e5b63bccd7c
Yair efron <yaire@MacBook-Air-sl-Yair.local>
2025-11-11 18:44:56 +0200

    this is the end

--- Cheers.java
+++ Cheers.java
@@ -2,7 +2,7 @@ public class Cheers {
         public static void main(String[] args) {
 	    String input=args[0];
             int num=Integer.parseInt(args[1]);
-            for(int i=0;i<input.length()-1;i++)
+            for(int i=0;i<input.length();i++)
             {
                 char cheked=input.charAt(i);
                 if (cheked=='A'||cheked=='E'||cheked=='F'||cheked=='H'||cheked=='I'||cheked=='L'||cheked=='M'||cheked=='N'||cheked=='O'||cheked=='R'||cheked=='S'||cheked=='X')
