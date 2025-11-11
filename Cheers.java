b7c7411d1cb77aeb458cda8eda6769809aeece03
Yair efron <yaire@MacBook-Air-sl-Yair.local>
2025-11-11 18:32:53 +0200

    first

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,23 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String input=args[0];
+            int num=Integer.parseInt(args[1]);
+            for(int i=0;i<input.length()-1;i++)
+            {
+                char cheked=input.charAt(i);
+                if (cheked=='A'||cheked=='E'||cheked=='F'||cheked=='H'||cheked=='I'||cheked=='L'||cheked=='M'||cheked=='N'||cheked=='O'||cheked=='R'||cheked=='S'||cheked=='X')
+                 {
+                        System.out.println("Give me an "+cheked+": "+cheked+"!");
+                }
+                else
+                {
+                        System.out.println("Give me a "+cheked+": "+cheked+"!");
+
+                }
+            }
+            for(int i=0;i<input.length()-1;i++)
+            {
+                System.out.println(input+"!!!");
+            }
         }
 }
