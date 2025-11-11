61ab46348c3bfcae188fc7618c4391ed487d3f88
Yair efron <yaire@MacBook-Air-sl-Yair.local>
2025-11-11 23:12:36 +0200

    1

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,25 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int input=Integer.parseInt(args[0]);
+		double sum=1;
+		double divider=3;
+		System.out.println(divider);
+		for(int i=1;i<input;i++)
+		{
+				if (i%2==1) 
+				{
+				sum=sum-(1.0/divider);
+				System.out.println(-(1.0/divider));
+				}
+				else
+				{ 
+					sum=sum+(1/divider);
+					System.out.println(1.0/divider);
+				}
+				divider=divider+2;
+		}
+		System.out.println("pi according to Java: "+Math.PI);
+		System.out.println("pi, approximated:     "+sum*4);
 	}
 }
